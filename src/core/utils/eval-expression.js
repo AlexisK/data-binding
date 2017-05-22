c40d1334c77c8b48e83a076b354d05fe029a54c5
Oleksii Kaliuzhnyi <alexey@stuccomedia.com>
2017-05-22 12:13:26 +0300

    refactor expression eval with mappings

--- src/core/utils/eval-expression.js
+++ src/core/utils/eval-expression.js
@@ -60,7 +60,7 @@ function addrWrite(obj, keyChain, value) {
     }
     let result = _addrWrite(obj, keyChain, value, 0);
 
-    if ( keyChain.length > 1) {
+    if ( keyChain.length > 1 ) {
         // trigger setter
         obj[keyChain[0]] = obj[keyChain[0]];
     }
@@ -90,41 +90,34 @@ function extractVars(workMap) {
 
 
 // Work by index
+
+const itemHandlers = {
+    def                  : () => null,
+    [TYPE.scope]         : (ctx, pair) => handleWork(ctx, pair[1]).result,
+    [TYPE.string]        : (ctx, pair) => pair[1],
+    [TYPE.number]        : (ctx, pair) => pair[1],
+    [TYPE.expression]    : (ctx, pair) => ctx[pair[1]],
+    [TYPE.expressionMap] : (ctx, pair) => addr(ctx, pair[1])
+};
+
 function handleItem(ctx, pair) {
     if ( !pair ) { return null; }
-    if ( pair[0] === TYPE.scope ) {
-        return handleWork(ctx, pair[1]).result;
-    } else if ( pair[0] === TYPE.string || pair[0] === TYPE.number ) {
-        return pair[1];
-    } else if ( pair[0] === TYPE.expression ) {
-        return ctx[pair[1]];
-    } else if ( pair[0] === TYPE.expressionMap ) {
-        return addr(ctx, pair[1]);
-    }
-    return null;
+    return (itemHandlers[pair[0]] || itemHandlers.def)(ctx, pair);
 }
 
-function handleOperator(result, pair, ctx, workMap, i) {
-    let leftOperand = result.pop();
+const operatorHandlers = {
+    [OPERATOR.plus]  : (result, pair, ctx, workMap, i) => result.push(result.pop() + handleItem(ctx, workMap[i])),
+    [OPERATOR.minus] : (result, pair, ctx, workMap, i) => result.push(result.pop() - handleItem(ctx, workMap[i])),
+    [OPERATOR.mult]  : (result, pair, ctx, workMap, i) => result.push(result.pop() * handleItem(ctx, workMap[i])),
+    [OPERATOR.div]   : (result, pair, ctx, workMap, i) => result.push(result.pop() / handleItem(ctx, workMap[i])),
+    [OPERATOR.perc]  : (result, pair, ctx, workMap, i) => result.push(result.pop() % handleItem(ctx, workMap[i])),
+    [OPERATOR.and]   : (result, pair, ctx, workMap, i) => result.push(result.pop() && handleItem(ctx, workMap[i])),
+    [OPERATOR.or]    : (result, pair, ctx, workMap, i) => result.push(result.pop() || handleItem(ctx, workMap[i])),
+    [OPERATOR.ass]   : (result, pair, ctx, workMap, i) => result.push(addrWrite(ctx, workMap[0][1], handleItem(ctx, workMap[i])))
+};
 
-    if ( pair[1] === OPERATOR.plus ) {
-        result.push(leftOperand + handleItem(ctx, workMap[i]));
-    } else if ( pair[1] === OPERATOR.minus ) {
-        result.push(leftOperand - handleItem(ctx, workMap[i]));
-    } else if ( pair[1] === OPERATOR.mult ) {
-        result.push(leftOperand * handleItem(ctx, workMap[i]));
-    } else if ( pair[1] === OPERATOR.div ) {
-        result.push(leftOperand / handleItem(ctx, workMap[i]));
-    } else if ( pair[1] === OPERATOR.perc ) {
-        result.push(leftOperand % handleItem(ctx, workMap[i]));
-    } else if ( pair[1] === OPERATOR.and ) {
-        result.push(leftOperand && handleItem(ctx, workMap[i]));
-    } else if ( pair[1] === OPERATOR.or ) {
-        result.push(leftOperand || handleItem(ctx, workMap[i]));
-    } else if ( pair[1] === OPERATOR.ass ) {
-        //console.log(workMap, leftOperand);
-        result.push(addrWrite(ctx, workMap[0][1], handleItem(ctx, workMap[i])));
-    }
+function handleOperator(result, pair, ctx, workMap, i) {
+    operatorHandlers[pair[1]](result, pair, ctx, workMap, i);
 }
 
 // Build work & index jumps
